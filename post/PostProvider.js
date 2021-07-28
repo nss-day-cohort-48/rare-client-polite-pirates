6473465a0fe4ed5e971a5111479827d8657e5618
Caleb Wagner <calebwagner5@gmail.com>
2021-07-28 09:18:10 -0500

    creating posts modules

---
+++ post/PostProvider.js
@@ -0,0 +1,23 @@
+import React, { useState, createContext } from "react";
+
+export const PostContext = createContext();
+
+export const PostProvider = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = () => {
+    return fetch("http:localhost:8088/posts")
+      .then((res) => res.json())
+      .then(setPosts);
+  };
+
+  return (
+    <PostContext.Provider
+      value={{
+        posts,
+      }}
+    >
+      {props.children}
+    </PostContext.Provider>
+  );
+};
