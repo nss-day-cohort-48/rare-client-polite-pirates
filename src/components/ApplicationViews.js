6778d1033c0c683d75a0e085b5d32056675049f1
Caleb Wagner <calebwagner5@gmail.com>
2021-07-28 14:49:46 -0500

    adding users and categories to posts

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,29 +1,30 @@
-import React from "react"
-import { Route } from "react-router-dom"
-import { CategoryList } from "./categories/CategoryList"
-import { CategoryProvider } from "./categories/CategoryProvider"
+import React from "react";
+import { Route } from "react-router-dom";
+import { CategoryList } from "./categories/CategoryList";
+import { CategoryProvider } from "./categories/CategoryProvider";
+import { PostList } from "./post/PostList";
+import { PostProvider } from "./post/PostProvider";
 
 export const ApplicationViews = () => {
-    return (
+  return (
     <>
-        <main style={{
-            margin: "5rem 2rem",
-            lineHeight: "1.75rem"
-        }}>
-        </main>
-            <PostProvider>
-            <CategoryProvider>
+      <main
+        style={{
+          margin: "5rem 2rem",
+          lineHeight: "1.75rem",
+        }}
+      ></main>
+      <PostProvider>
+        <CategoryProvider>
+          <Route exact path="/categories">
+            <CategoryList />
+          </Route>
 
-                <Route exact path="/categories">
-                    <CategoryList />
-                </Route>
-
-                <Route exact path="/posts">
-                    <PostList />
-                </Route>
-
-            </CategoryProvider>
-            </PostProvider>
+          <Route exact path="/posts">
+            <PostList />
+          </Route>
+        </CategoryProvider>
+      </PostProvider>
     </>
-    )
+  );
 };
--- src/components/post/PostDetail.js
+++ src/components/post/PostDetail.js
@@ -5,8 +5,10 @@ export const PostDetail = ({ post }) => {
   return (
     <section className="post">
       <h3 className="post__title">{post.title}</h3>
-      <div className="post__author">Author: {post.user_id}</div>
-      <div className="post__category">Category: {post.user_id}</div>
+      <div className="post__author">
+        Author: {post.user?.first_name} {post.user?.last_name}
+      </div>
+      <div className="post__category">Category: {post.category?.label}</div>
     </section>
   );
 };
