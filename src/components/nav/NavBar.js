a62850f3c84d86a505cdc887f7c22e53d1b9ebda
peytonkemp <peyton.kemp@gmail.com>
2021-07-28 10:28:16 -0500

    added category manager to navbar and added some navbar css

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -12,7 +12,10 @@ export const NavBar = () => {
                 <img className="navbar__logo" src={Logo} />
             </li>
             <li className="navbar__item">
-                <Link className="navbar__link" to="/">Posts</Link>
+                <Link className="navbar__link" to="/posts">Posts</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/categories">Category Manager</Link>
             </li>
             {
                 (localStorage.getItem("rare_user_id") !== null) ?
