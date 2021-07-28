e6d6ba4bd3771f0d881d9906202fc50e3000bb61
Caleb Wagner <calebwagner5@gmail.com>
2021-07-28 10:55:57 -0500

    created route to posts

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,12 +1,22 @@
-import React from "react"
-import { Route } from "react-router-dom"
+import React from "react";
+import { Route } from "react-router-dom";
+import { PostList } from "./post/PostList";
+import { PostProvider } from "./post/PostProvider";
 
 export const ApplicationViews = () => {
-    return <>
-        <main style={{
-            margin: "5rem 2rem",
-            lineHeight: "1.75rem"
-        }}>
-        </main>
+  return (
+    <>
+      <main
+        style={{
+          margin: "5rem 2rem",
+          lineHeight: "1.75rem",
+        }}
+      ></main>
+      <PostProvider>
+        <Route exact path="/posts">
+          <PostList />
+        </Route>
+      </PostProvider>
     </>
-}
+  );
+};
