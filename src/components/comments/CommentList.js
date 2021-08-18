abc00b7f4b75ed63ad3f8e7fc58b28ffebc9444e
wooldridge12 <wooldridgemikey@gmail.com>
2021-08-18 12:41:47 -0400

    Most of CommentList.js is finished

--- src/components/comments/CommentList.js
+++ src/components/comments/CommentList.js
@@ -13,7 +13,34 @@ export const CommentList =(props) => {
 
     return (
         <article className="comments">
-            
+            <header className="comments__header">
+
+              <h1>My Comment</h1>
+
+              <button className="btn btn-2 btn-sep icon create" onClick={() => {
+                  history.push ({ pathname: "/comments/new"})
+              }}>New Comment</button>  
+            </header>
+            {
+                comments.map(comment => {
+                    return <section key={comment.id} className="registration">
+                        <div className="registration__comment_content">{comment.content}</div>
+                        <div className="registration__comment_author">{comment.author}</div>
+                        <div>
+                            Time: {
+                                new Date(comment.date).toLocaleDateString("en-US",
+                                {
+                                    weekday: 'long',
+                                    year: 'numeric',
+                                    month: 'long',
+                                    day: 'numeric'
+                                })
+                            }
+                            @ {comment.time}
+                        </div>
+                    </section>
+                })
+            }
         </article>
     )
 }
