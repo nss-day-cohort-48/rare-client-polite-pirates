140ee6c4e6162c79df5525b7c397946cc903b0fa
Jaclyn French <jaclynmariefrench@gmail.com>
2021-08-20 11:24:30 -0500

    got details to show up

--- src/components/post/PostContent.js
+++ src/components/post/PostContent.js
@@ -2,11 +2,13 @@ import React, { useContext, useEffect, useState} from "react";
 import { useParams } from "react-router-dom"
 import { PostContext } from "./PostProvider";
 import date from "date-and-time"
+import { HumanDate } from "../utils/HumanDate";
 // import "./Post.css";
 
 export const PostContent = () => {
   const { getPostById } = useContext(PostContext)
   const [ post, setPost ] = useState( {} )
+  const [time, setTime] = useState("")
 
   const {postId} = useParams()
 
@@ -16,10 +18,15 @@ export const PostContent = () => {
     })
 }, [])
 
-  
+  useEffect(()=> {
+    if("publication_date" in post){
+      const time = post.publication_date
+      const converted_time = HumanDate(time)
+      setTime(converted_time)
+    }
 
-  const post_time = new Date()
-  const converted_time = date.format(post_time, 'MM/DD/YYYY')
+  }, [post])
+
 
   return (
     <section className="post">
@@ -27,7 +34,7 @@ export const PostContent = () => {
       {/* <div><img src={post.image_url}/></div> */}
       <div className="post__content">Content: {post.content}</div>
       <div className="post__category">Category: {post.category?.label}</div>
-      <div className="post__publication_date">Posted on: {converted_time}</div>
+      <div className="post__publication_date">Posted on: {time}</div>
       <div className="post__author">
       Author: {post.rare_user?.user?.first_name} {post.rare_user?.user?.last_name}
       </div>
